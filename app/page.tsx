0083587e79041a5757621d17528fc11453b2fafb
jacesoul <seolju90@gmail.com>
2025-05-19 12:37:40 -0500

    Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,25 @@
 export default function Home() {
   return (
     <main className="bg-gray-200 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 xl:bg-orange-100 2xl:bg-purple-100 h-screen flex items-center justify-center p-5 dark:bg-gray-700">
-      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen dark:bg-gray-600 flex flex-col md:flex-row gap-2 *:outline-none ring ring-transparent transition-shadow has-[:invalid]:ring-red-100">
-        <input
-          className="w-full rounded-full py-3 bg-gray-200 dark:bg-gray-300 pl-5 ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          type="email"
-          placeholder="text"
-          required
-        />
-        <span className="text-red-500 py-2 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className="bg-black dark:bg-gray-500 text-white dark:text-gray-300 py-2 rounded-full active:scale-90 transition-transform font-medium md:px-10">
-          Log in
-        </button>
+      <div className="bg-white shadow-lg p-5 w-full rounded-3xl max-w-screen  flex flex-col gap-4">
+        {["Jace", "Me", "You", "Yourself", ""].map((person, index) => {
+          return (
+            <div
+              key={index}
+              className="flex items-center gap-3 odd:bg-gray-100 even:bg-green-100 p-2.5 rounded-xl border-b-2 pb-3 last:border-b-0"
+            >
+              <div className="size-9 bg-blue-400 rounded-full animate-pulse"></div>
+              {/* <div className="w-20 h-3 rounded-full bg-gray-500 animate-pulse"></div> */}
+              <span className="text-lg font-medium empty:w-24 empty:h-3 empty:rounded-full empty:animate-pulse empty:bg-gray-300">
+                {person}
+              </span>
+              <div className="size-6 bg-red-400 rounded-full text-white flex items-center justify-center relative">
+                <span className="z-10">{index}</span>
+                <div className="size-6 bg-red-400 rounded-full absolute animate-ping"></div>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
