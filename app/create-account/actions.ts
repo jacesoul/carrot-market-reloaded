437155a144ce49b7be13b154f668f6ff1c995615
jacesoul <seolju90@gmail.com>
2025-05-28 15:27:03 -0500

    Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,13 +2,6 @@
 
 import { z } from "zod";
 
-const validateUsername = (username: string) => {
-  if (username.includes("@")) {
-    return true;
-  }
-  return false;
-};
-
 const validatePassword = ({
   password,
   confirm_password,
@@ -16,6 +9,9 @@ const validatePassword = ({
   password: string;
   confirm_password: string;
 }) => password === confirm_password;
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{10,}$/;
 
 const formSchema = z
   .object({
@@ -26,9 +22,14 @@ const formSchema = z
       })
       .min(3, "Way too short")
       .max(10, "Way too long")
-      .refine(validateUsername, "Username must contain @"),
-    email: z.string().email("Invalid email"),
-    password: z.string().min(10, "Way too short"),
+      .transform((username) => username.toLowerCase().trim()),
+    email: z.string().email("Invalid email").toLowerCase().trim(),
+    password: z
+      .string()
+      .regex(
+        passwordRegex,
+        "Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character"
+      ),
     confirm_password: z.string().min(10, "Way too short"),
   })
   .refine(validatePassword, {
@@ -49,5 +50,8 @@ export async function createAccount(prevState: any, formData: FormData) {
   if (!result.success) {
     console.log(result.error.flatten());
     return result.error.flatten();
+  } else {
+    console.log(result.data);
+    return result.data;
   }
 }
