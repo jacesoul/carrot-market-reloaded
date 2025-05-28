b5ce6ce36a69cf41e287d9ad5c28348003bc3235
jacesoul <seolju90@gmail.com>
2025-05-28 15:07:50 -0500

    Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,39 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(10),
-  email: z.string().email(),
-  password: z.string().min(10),
-  confirm_password: z.string().min(10),
-});
+const validateUsername = (username: string) => {
+  if (username.includes("@")) {
+    return true;
+  }
+  return false;
+};
+
+const validatePassword = ({
+  password,
+  confirm_password,
+}: {
+  password: string;
+  confirm_password: string;
+}) => password === confirm_password;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "Username must be a string",
+        required_error: "Username is required",
+      })
+      .min(3, "Way too short")
+      .max(10, "Way too long")
+      .refine(validateUsername, "Username must contain @"),
+    email: z.string().email("Invalid email"),
+    password: z.string().min(10, "Way too short"),
+    confirm_password: z.string().min(10, "Way too short"),
+  })
+  .refine(validatePassword, {
+    message: "Passwords do not match",
+    path: ["confirm_password"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
@@ -20,6 +47,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
 
   if (!result.success) {
+    console.log(result.error.flatten());
     return result.error.flatten();
   }
 }
