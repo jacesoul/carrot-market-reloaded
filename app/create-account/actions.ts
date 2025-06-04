ce950e88fef29f022384bb82108c7a36acbf3c1a
jacesoul <seolju90@gmail.com>
2025-06-04 13:58:57 -0500

    Database Validation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -5,6 +5,7 @@ import {
   PASSWORD_REGEX,
   PASSWORD_VALIDATION_MESSAGE,
 } from "@/lib/constants";
+import prisma from "@/lib/db";
 import { z } from "zod";
 
 const validatePassword = ({
@@ -15,6 +16,29 @@ const validatePassword = ({
   confirm_password: string;
 }) => password === confirm_password;
 
+const checkUniqueUsername = async (username: string) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      username,
+    },
+    select: {
+      id: true,
+    },
+  });
+
+  return !Boolean(user);
+};
+
+const checkUniqueEmail = async (email: string) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  return !Boolean(user);
+};
+
 const formSchema = z
   .object({
     username: z
@@ -22,8 +46,18 @@ const formSchema = z
         invalid_type_error: "Username must be a string",
         required_error: "Username is required",
       })
-      .transform((username) => username.toLowerCase().trim()),
-    email: z.string().email("Invalid email").toLowerCase().trim(),
+      .transform((username) => username.toLowerCase().trim())
+      .refine(checkUniqueUsername, {
+        message: "Username already in use",
+      }),
+    email: z
+      .string()
+      .email("Invalid email")
+      .toLowerCase()
+      .trim()
+      .refine(checkUniqueEmail, {
+        message: "Email already in use",
+      }),
     password: z.string().regex(PASSWORD_REGEX, PASSWORD_VALIDATION_MESSAGE),
     confirm_password: z.string().min(PASSWORD_MIN_LENGTH),
   })
@@ -40,11 +74,11 @@ export async function createAccount(prevState: any, formData: FormData) {
     confirm_password: formData.get("confirm_password"),
   };
 
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
 
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const { username, email, password } = result.data;
   }
 }
