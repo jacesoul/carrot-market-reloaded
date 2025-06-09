d8600616503be5baf252db27fa68070e4fd7cfaf
jacesoul <seolju90@gmail.com>
2025-06-09 17:26:53 -0500

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,24 @@
-import { NextRequest } from "next/server";
-import getSession from "./lib/session";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
+export default async function middleware(request: NextRequest) {
+  console.log("Hello!!!");
+  const pathName = request.nextUrl.pathname;
+  if (pathName === "/") {
+    const response = NextResponse.next();
+    response.cookies.set("middleware-cookie", "hello");
+
+    return response;
+  }
+
+  //   const session = await getSession();
+  //   console.log(session);
   if (request.nextUrl.pathname === "/profile") {
     return Response.redirect(new URL("/login", request.url));
   }
 }
+
+export const config = {
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
+};
