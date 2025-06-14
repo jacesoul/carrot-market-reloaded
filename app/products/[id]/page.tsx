311cdf06839bfed5ea83a49997e9ea24b006eb08
jacesoul <seolju90@gmail.com>
2025-06-14 16:15:31 -0500

    Product Detail Page

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,12 +1,100 @@
-async function getProduct() {
-  await new Promise((resolve) => setTimeout(resolve, 60000));
+import prisma from "@/lib/db";
+import getSession from "@/lib/session";
+import { formatToWon } from "@/lib/utils";
+import { UserIcon } from "@heroicons/react/16/solid";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+async function getIsOwner(userId: number) {
+  const session = await getSession();
+  if (session.id) {
+    return session.id === userId;
+  }
+  return false;
+}
+
+async function getProduct(id: number) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      user: {
+        select: {
+          username: true,
+          avatar: true,
+        },
+      },
+    },
+  });
+
+  return product;
 }
 
 export default async function ProductDetail({
-  params: { id },
+  params,
 }: {
   params: { id: string };
 }) {
-  const product = await getProduct();
-  return <span>Product detail of the product {id}</span>;
+  const id = Number(params.id);
+
+  if (isNaN(id)) {
+    return notFound();
+  }
+
+  const product = await getProduct(id);
+  if (!product) {
+    return notFound();
+  }
+
+  const isOwner = await getIsOwner(product.userId);
+
+  return (
+    <div>
+      <div className="relative aspect-square">
+        <Image fill src={product.photo} alt={product.title} />
+      </div>
+      <div className="p-5">
+        <div className=" flex items-center gap-3 border-b border-neutral-700">
+          <div className="size-10 rounded-full">
+            {product.user.avatar !== null ? (
+              <Image
+                src={product.user.avatar}
+                width={40}
+                height={40}
+                alt={product.user.username}
+                className="rounded-full object-cover"
+              />
+            ) : (
+              <UserIcon />
+            )}
+          </div>
+          <div>
+            <h3>{product.user.username}</h3>
+          </div>
+        </div>
+        <div>
+          <h1 className="text-2xl font-semibold">{product.title}</h1>
+          <p>{product.description}</p>
+        </div>
+      </div>
+      <div className="fixed w-full bottom-0 left-0 p-5 pb-10 bg-neutral-800 flex justify-between items-center">
+        <span className="font-semibold text-xl">
+          {formatToWon(product.price)}원
+        </span>
+        {isOwner ? (
+          <button className="bg-red-500 px-5 py-2.5 rounded-md text-white font-semibold">
+            Delete Product
+          </button>
+        ) : null}
+        <Link
+          className="bg-orange-500 px-5 py-2.5 rounded-md text-white font-semibold"
+          href={``}
+        >
+          Go to Chat
+        </Link>
+      </div>
+    </div>
+  );
 }
