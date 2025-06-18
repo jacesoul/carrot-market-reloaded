cb0a0db3514080237f8d83f148e5505a67b59eea
jacesoul <seolju90@gmail.com>
2025-06-18 16:05:18 -0500

    Edit Infinite Scrolling

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -33,8 +33,8 @@ export default function ProductList({ initialProducts }: ProductListProps) {
           const newProducts = await getMoreProducts(page + 1);
 
           if (newProducts.length !== 0) {
-            setPage((prev) => prev + 1);
             setProducts((prev) => [...prev, ...newProducts]);
+            setPage((prev) => prev + 1);
           } else {
             setIsLastPage(true);
           }
@@ -67,10 +67,7 @@ export default function ProductList({ initialProducts }: ProductListProps) {
       ) : (
         <span
           ref={trigger}
-          style={{
-            marginTop: `${page + 1 * 900}vh`,
-          }}
-          className="mb-96 text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
+          className="text-sm font-semibold bg-orange-500 w-fit mx-auto px-3 py-2 rounded-md hover:opacity-90 active:scale-95"
         >
           {isLoading ? "Loading..." : "Load more"}
         </span>
