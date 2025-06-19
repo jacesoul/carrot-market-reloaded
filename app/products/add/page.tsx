eb82a6f5eba07f959ed26ef76c6dd170da09b306
jacesoul <seolju90@gmail.com>
2025-06-19 10:07:30 -0500

    Product Upload in local

--- app/products/add/page.tsx
+++ app/products/add/page.tsx
@@ -5,6 +5,7 @@ import Input from "@/components/input";
 import { PhotoIcon } from "@heroicons/react/24/solid";
 import { useState } from "react";
 import { uploadProduct } from "./actions";
+import { useFormState } from "react-dom";
 
 export default function AddProduct() {
   const [preview, setPreview] = useState("");
@@ -23,9 +24,11 @@ export default function AddProduct() {
     setPreview(url);
   };
 
+  const [state, action] = useFormState(uploadProduct, null);
+
   return (
     <div>
-      <form action={uploadProduct} className="p-5 flex flex-col gap-5">
+      <form action={action} className="p-5 flex flex-col gap-5">
         <label
           htmlFor="photo"
           className="border-2 aspect-square flex items-center justify-center flex-col text-neutral-300 border-neutral-300 rounded-md border-dashed cursor-pointer bg-center bg-cover"
@@ -38,6 +41,7 @@ export default function AddProduct() {
               <PhotoIcon className="w-20" />
               <div className="text-neutral-400 text-sm">
                 사진을 추가해주세요.
+                {state?.fieldErrors.photo}
               </div>
             </>
           ) : null}
@@ -50,13 +54,26 @@ export default function AddProduct() {
           accept="image/*"
           className="hidden"
         />
-        <Input type="text" name="title" required placeholder="제목" />
-        <Input type="number" name="price" required placeholder="가격" />
+        <Input
+          type="text"
+          name="title"
+          required
+          placeholder="제목"
+          errors={state?.fieldErrors.title}
+        />
+        <Input
+          type="number"
+          name="price"
+          required
+          placeholder="가격"
+          errors={state?.fieldErrors.price}
+        />
         <Input
           type="text"
           name="description"
           required
           placeholder="자세한 설명"
+          errors={state?.fieldErrors.description}
         />
         <Button text="작성 완료" />
       </form>
