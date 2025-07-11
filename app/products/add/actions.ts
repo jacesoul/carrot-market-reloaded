453d0b06769b5bb766b9b64ba389fdaea578826b
jacesoul <seolju90@gmail.com>
2025-07-11 16:48:55 -0600

    Upload URLs

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -29,14 +29,6 @@ export async function uploadProduct(_: any, formData: FormData) {
     photo: formData.get("photo"),
   };
 
-  if (data.photo instanceof File) {
-    const photoData = await data.photo.arrayBuffer();
-
-    await fs.appendFile(`./public/${data.photo.name}`, Buffer.from(photoData));
-
-    data.photo = `/${data.photo.name}`;
-  }
-
   const result = productSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
@@ -63,4 +55,20 @@ export async function uploadProduct(_: any, formData: FormData) {
       redirect(`/products/${product.id}`);
     }
   }
+}
+
+export async function getUploadUrl() {
+  const response = await fetch(
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`,
+      },
+    }
+  );
+
+  const data = await response.json();
+
+  return data;
 }
